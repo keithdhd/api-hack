16ff85002deb6863703dc6ef1ac8e7db0d5868aa
keithdhd <keith@keithdouglas.co.uk>
2015-04-29 11:48:27 +0100

    initial commit

---
+++ js/app.js
@@ -0,0 +1,90 @@
+$(function(){
+
+	$("#search-button").click(function(event){
+		$("#results").html("");
+		$("#results").hide();
+
+		var searchQuery = $("#input-search").val().trim();
+		getSearchResults(searchQuery);
+	});
+
+})
+
+var getSearchResults = function(searchQuery){
+
+	var params = {
+		q: searchQuery,
+		type: "artist"
+	};
+
+	var results = $.ajax({
+		url: "https://api.spotify.com/v1/search",
+		data: params,
+		dataType: "json",
+		type: "GET"
+	})
+	.done(function(result){
+		showSearchResults(result);
+	})
+	.fail(function(jqXHR, error, errorThrown){
+		var errorElem = showError(error);
+		$('.results').append(errorElem);
+	});
+}
+
+var showSearchResults = function(result){
+	
+	var artistTopTen = [];
+	
+	$.each(result.artists.items, function(i, item){
+
+		//get the top 10 for this artist
+		artistTopTen = getTopTen(item);
+
+		// clone our result template code
+		var resultsTemplate = $(".templates .artist").clone();
+
+		var cover = resultsTemplate.find(".cover");
+		var artistName = resultsTemplate.find(".artist-name");
+		artistName.text(item.name);
+		
+		if(item.images.length){
+			var imageUrl = item.images[0].url;
+		}else
+			var imageUrl = "default image url";
+
+		cover.css({
+			"background-image": "url(" + imageUrl + ")",
+			"background-size": "cover"
+		});
+
+		var topTenTemplate = resultsTemplate.find(".top-ten");
+		topTenTemplate.text(artistTopTen.length);
+
+		$("#results").append(resultsTemplate).fadeIn(3000);
+	});
+
+}
+var getTopTen = function(artist){
+
+	var results = $.ajax({
+		url: "https://api.spotify.com/v1/artists/" + artist.id + "/top-tracks?country=US",
+		dataType: "json",
+		type: "GET"
+	})
+	.done(function(results){
+		return results;
+	})
+	.fail(function(jqXHR, error, errorThrown){
+		var errorElem = showError(error);
+		$('.results').append(errorElem);
+	});
+
+}
+
+var showError = function(error){
+	// clone our error template code
+	var errorTemplate = $(".templates .error").clone();
+	errorTemplate.text(error);
+	return errorTemplate;
+}
